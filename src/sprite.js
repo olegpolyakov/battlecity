1fdd78adf484193d79a0ac12d756bc963979064d
Oleg Polyakov <olegpolyakov@outlook.com>
2021-03-02 22:09:35 +0300

    Add sprite map to Sprite

--- src/sprite.js
+++ src/sprite.js
@@ -1,7 +1,18 @@
 export default class Sprite {
-    constructor(src) {
+    constructor(src, map) {
         this.src = src;
         this.image = new Image();
+        this.map = map;
+    }
+
+    set(id, { x, y, width, height }) {
+        this.map[id]([x, y, width, height]);
+
+        return this;
+    }
+
+    get(id) {
+        return this.map[id];
     }
 
     async load() {
