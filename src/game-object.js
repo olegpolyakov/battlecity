4a4e14896e78b579e4873b12581fae3813c905d9
Oleg Polyakov <olegpolyakov@outlook.com>
2021-06-13 22:10:34 +0800

    Add setters

--- src/game-object.js
+++ src/game-object.js
@@ -4,16 +4,48 @@ export default class GameObject extends EventEmitter {
     constructor({ x, y, width, height, sprites } = {}) {
         super();
 
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
         this.sprites = sprites;
         this.animationFrame = 0;
         this.animationSpeed = 0;
         this.frames = 0;
         this.isDestructable = false;
         this.isDestroyed = false;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set x(value) {
+        this._x = value;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    set y(value) {
+        this._y = value;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    set width(value) {
+        this._width = value;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    set height(value) {
+        this._height = value;
     }
 
     get top() {
